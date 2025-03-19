17951d2159e1874b9e0149d138fa1c9d5de41869
Edgar Fuentes <edgar.fuentes@udc.es>
2025-03-19 23:02:59 +0100

    Knn con gridsearch

--- modelos/KNN.py
+++ modelos/KNN.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.pipeline import Pipeline
 
 def knn(X_train_rgb, y_train_encoded, X_test_rgb, y_test_encoded):
     X_train_flattened = X_train_rgb.reshape(X_train_rgb.shape[0], -1)
@@ -73,3 +76,43 @@ def knn(X_train_rgb, y_train_encoded, X_test_rgb, y_test_encoded):
     # Guardar distancias y predicciones en un archivo Excel
     # save_to_excel(accuracy_train, accuracy_test, aciertos_train, aciertos_test, len(y_train_pred), len(y_test_pred), filename="knn_results.xlsx")
 
+def knn_with_gridsearch(X_train_rgb, y_train_encoded, X_test_rgb, y_test_encoded):
+    # Aplanar las imágenes para convertirlas en una matriz 2D
+    X_train_flattened = X_train_rgb.reshape(X_train_rgb.shape[0], -1)
+    X_test_flattened = X_test_rgb.reshape(X_test_rgb.shape[0], -1)
+
+    # Escalar las características
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train_flattened)
+    X_test_scaled = scaler.transform(X_test_flattened)
+
+    # Definir el espacio de búsqueda de los hiperparámetros
+    param_grid = {
+        'pca__n_components': [30, 50],  # Número de componentes principales
+        'knn__n_neighbors': [3, 7]  # Número de vecinos para el clasificador KNN
+    }
+
+    # Crear un pipeline para aplicar PCA seguido de KNN
+    pipeline = Pipeline([
+        ('pca', PCA()),  # PCA en el pipeline
+        ('knn', KNeighborsClassifier())  # KNN en el pipeline
+    ])
+
+    # Usar GridSearchCV para buscar los mejores hiperparámetros
+    grid_search = GridSearchCV(pipeline, param_grid, cv=5)  # Validación cruzada
+    grid_search.fit(X_train_scaled, y_train_encoded)
+
+    # Imprimir los mejores parámetros encontrados
+    print(f"Mejores parámetros: {grid_search.best_params_}")
+
+    # Obtener el mejor modelo entrenado con los mejores hiperparámetros
+    best_model = grid_search.best_estimator_
+
+    # Extraer la mejor instancia de PCA y KNN
+    best_pca = best_model.named_steps['pca']
+    best_knn = best_model.named_steps['knn']
+    results = pd.DataFrame(grid_search.cv_results_)[['param_pca__n_components', 'param_knn__n_neighbors', 'mean_test_score']]
+    print(results.sort_values(by='mean_test_score', ascending=False))  # Ordenar de mejor a peor
+
+    # Devolver el modelo KNN y la instancia de PCA como una tupla (igual que la función knn)
+    return best_knn, best_pca
