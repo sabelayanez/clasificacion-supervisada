7360f7227062f53a52aab23426456231e571e3a0
Edgar Fuentes <edgar.fuentes@udc.es>
2025-03-19 19:39:00 +0100

    Más parámetros para Arbol Decision

--- modelos/ArbolDeDecision.py
+++ modelos/ArbolDeDecision.py
@@ -8,15 +8,15 @@ def arbol_decision(X_train, y_train):
     X_train_flat = X_train.reshape(X_train.shape[0], -1)  # De 4D a 2D
 
     param_grid = {
-        'max_depth': [10],  # Solo un valor
-        'min_samples_split': [2],  # Solo un valor
-        'min_samples_leaf': [1],  # Solo un valor
-        'criterion': ['gini']  # Solo un valor
+        'max_depth': [10, 20, None],  # Profundidad máxima del árbol
+        'min_samples_split': [2, 5, 10],  # Mínimo número de muestras para dividir un nodo
+        'min_samples_leaf': [1, 2, 4],  # Mínimo número de muestras en un nodo hoja
+        'criterion': ['gini', 'entropy'],  # Función de división
     }
 
 
     # Búsqueda de los mejores parámetros utilizando GridSearchCV
-    grid_search = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5)
+    grid_search = GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid, cv=5) #Validación cruzada
     grid_search.fit(X_train_flat, y_train)
 
     print(f"Mejores parámetros: {grid_search.best_params_}")
