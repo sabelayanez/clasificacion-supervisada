060538726b50e51b01774139708e3c453ac13643
Edgar Fuentes <edgar.fuentes@udc.es>
2025-03-19 21:10:31 +0100

    First commit

---
+++ modelos/RandomForest.py
@@ -0,0 +1,80 @@
+from sklearn.ensemble import RandomForestClassifier
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Model
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import PCA
+from skimage.feature import hog
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+def rforest_vgg16_pca(X_train, y_train, X_test, input_shape=(256, 256, 3), n_components=500):
+    
+    base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
+    feature_extractor = Model(inputs=base_model.input, outputs=base_model.output)
+
+    # Extraer características con VGG16
+    X_train_features = feature_extractor.predict(X_train)
+    X_test_features = feature_extractor.predict(X_test)
+
+    # Aplanar las características extraídas
+    X_train_features_flat = X_train_features.reshape(X_train_features.shape[0], -1)
+    X_test_features_flat = X_test_features.reshape(X_test_features.shape[0], -1)
+    
+    pca = PCA(n_components=500, svd_solver='randomized')  # Elegimos 200 características más relevantes
+    X_train_pca = pca.fit_transform(X_train_features_flat)
+    X_test_pca = pca.transform(X_test_features_flat)
+
+    model_rf = RandomForestClassifier(
+    n_estimators=200,  # Más árboles = mejor generalización
+    max_depth=30,  # Mayor profundidad
+    min_samples_split=3,  # Menos datos necesarios para dividir
+    min_samples_leaf=2,  # Evita ramas muy pequeñas
+    random_state=42
+    )
+    model_rf.fit(X_train_pca, y_train)
+
+    # Precisión con VGG16 + PCA
+    return model_rf
+
+def extract_hog_features(images):
+
+    hog_features = []
+    for img in images:
+        feature = hog(img, pixels_per_cell=(16, 16), cells_per_block=(2, 2), feature_vector=True)
+        hog_features.append(feature)
+
+    return np.array(hog_features)
+
+def rforest_vgg16_pca_hog(X_train, y_train, X_test, X_train_gray, X_test_gray, input_shape=(256,256,3), n_components=500):
+    base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
+    feature_extractor = Model(inputs=base_model.input, outputs=base_model.output)
+
+    # Extraer características con VGG16
+    X_train_features = feature_extractor.predict(X_train)
+    X_test_features = feature_extractor.predict(X_test)
+
+    # Aplanar las características extraídas
+    X_train_features_flat = X_train_features.reshape(X_train_features.shape[0], -1)
+    X_test_features_flat = X_test_features.reshape(X_test_features.shape[0], -1)
+    
+    pca = PCA(n_components=500, svd_solver='randomized')  # Elegimos 200 características más relevantes
+    X_train_pca = pca.fit_transform(X_train_features_flat)
+    X_test_pca = pca.transform(X_test_features_flat)
+
+    # Extraer características HOG
+    X_train_hog = extract_hog_features(X_train_gray)
+    X_test_hog = extract_hog_features(X_test_gray)
+    # Normalizar características
+    scaler = StandardScaler()
+    X_train_hog_scaled = scaler.fit_transform(X_train_hog)
+    X_test_hog_scaled = scaler.transform(X_test_hog)
+
+    # Concatenar VGG16 + HOG
+    X_train_combined = np.hstack((X_train_pca, X_train_hog_scaled))
+    X_test_combined = np.hstack((X_test_pca, X_test_hog_scaled))
+
+    # Entrenar Random Forest con características combinadas
+    model_rf_combined = RandomForestClassifier(n_estimators=200, max_depth=30, random_state=42)
+    model_rf_combined.fit(X_train_combined, y_train)
+
+    return model_rf_combined
