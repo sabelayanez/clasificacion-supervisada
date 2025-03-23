629c06906cfd6eddf102fc49183a3e1ca965c2c2
Edgar Fuentes <edgar.fuentes@udc.es>
2025-03-23 16:59:22 +0100

    Modelos actualizados con función validacion

--- modelos/KNN.py
+++ modelos/KNN.py
@@ -8,6 +8,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_validate
 
 from constants import CV, scoring
+from utils import validacion
 
 def knn(X_train_rgb, y_train_encoded, X_test_rgb):
     X_train_flattened = X_train_rgb.reshape(X_train_rgb.shape[0], -1)
@@ -30,7 +31,7 @@ def knn(X_train_rgb, y_train_encoded, X_test_rgb):
 
 
 def knn_with_gridsearch(X_train_rgb, y_train_encoded, X_test_rgb, y_test_encoded):
-    # Aplanar las imágenes para convertirlas en una matriz 2D
+    # Aplanar las imágenes
     X_train_flattened = X_train_rgb.reshape(X_train_rgb.shape[0], -1)
     X_test_flattened = X_test_rgb.reshape(X_test_rgb.shape[0], -1)
 
@@ -39,36 +40,39 @@ def knn_with_gridsearch(X_train_rgb, y_train_encoded, X_test_rgb, y_test_encoded
     X_train_scaled = scaler.fit_transform(X_train_flattened)
     X_test_scaled = scaler.transform(X_test_flattened)
 
-    # Definir el espacio de búsqueda de los hiperparámetros
+    # Definir el espacio de búsqueda de hiperparámetros
     param_grid = {
-        'pca__n_components': [30, 50],  # Número de componentes principales
-        'knn__n_neighbors': [3, 7]  # Número de vecinos para el clasificador KNN
+        'pca__n_components': [30, 50],
+        'knn__n_neighbors': [3, 7]
     }
 
-    # Crear un pipeline para aplicar PCA seguido de KNN
+    # Crear un pipeline
     pipeline = Pipeline([
-        ('pca', PCA()),  # PCA en el pipeline
-        ('knn', KNeighborsClassifier())  # KNN en el pipeline
+        ('pca', PCA()),
+        ('knn', KNeighborsClassifier())
     ])
 
-    # Usar GridSearchCV para buscar los mejores hiperparámetros
-    grid_search = GridSearchCV(pipeline, param_grid, cv=CV, scoring=scoring, refit='accuracy')  # Validación cruzada
+    # Aplicar GridSearchCV
+    grid_search = GridSearchCV(pipeline, param_grid, cv=CV, scoring=scoring, refit='accuracy')
     grid_search.fit(X_train_scaled, y_train_encoded)
 
-    # Imprimir los mejores parámetros encontrados
     print(f"Mejores parámetros: {grid_search.best_params_}")
 
-    # Obtener el mejor modelo entrenado con los mejores hiperparámetros
+    # Obtener el mejor modelo
     best_model = grid_search.best_estimator_
-
-    # Extraer la mejor instancia de PCA y KNN
     best_pca = best_model.named_steps['pca']
     best_knn = best_model.named_steps['knn']
-    results = pd.DataFrame(grid_search.cv_results_)[[
-        'param_pca__n_components', 'param_knn__n_neighbors', 
-        'mean_test_accuracy', 'mean_test_precision', 'mean_test_recall', 'mean_test_f1', 'mean_test_roc_auc'
-    ]]
-    print(results.sort_values(by='mean_test_accuracy', ascending=False))  # Ordenar de mejor a peor
 
-    return best_knn, best_pca, results
+    results = pd.DataFrame(grid_search.cv_results_)[[
+        'param_pca__n_components', 'param_knn__n_neighbors',
+        'mean_test_accuracy', 'mean_test_precision', 'mean_test_recall',
+        'mean_test_f1', 'mean_test_roc_auc'
+    ]]
+
+    print(results.sort_values(by='mean_test_accuracy', ascending=False))
+    #visualize_knn_predictions(best_knn, best_pca, scaler, X_test_rgb_64, y_test_encoded, class_names)
+    y_pred_knn = grid_search.predict(X_test_scaled)
+    validacion(X_test_rgb, y_test_encoded, y_pred_knn)
+
+    return best_knn, best_pca, results 
 
--- modelos/RandomForest.py
+++ modelos/RandomForest.py
@@ -6,29 +6,34 @@ from sklearn.model_selection import cross_validate
 from sklearn.decomposition import PCA
 from skimage.feature import hog
 from sklearn.preprocessing import StandardScaler
-
+from utils import validacion
 from constants import scoring, CV
-
+import matplotlib.pyplot as plt
+import random
 import numpy as np
 
-def random_forest(X_train, y_train, X_test):
-    # Crear y entrenar el modelo Random Forest
-    
+def random_forest(X_train, y_train_encoded, X_test, y_test_encoded, class_names):
+    # Aplanar imágenes
     X_train_flat = X_train.reshape(X_train.shape[0], -1)
     X_test_flat = X_test.reshape(X_test.shape[0], -1)
-    
+
+    # Crear y entrenar el modelo Random Forest
     modelRF = RandomForestClassifier(n_estimators=100, random_state=42)
-    modelRF.fit(X_train_flat, y_train)
+    modelRF.fit(X_train_flat, y_train_encoded)
 
     # Hacer predicciones
     y_pred_rf = modelRF.predict(X_test_flat)
 
     # Validación cruzada
-    scoresRF = cross_validate(modelRF, X_train_flat, y_train, cv=CV, scoring=scoring)
+    scoresRF = cross_validate(modelRF, X_train_flat, y_train_encoded, cv=5, scoring='accuracy')
 
-    return modelRF, scoresRF
+    # Visualizar imágenes y predicciones
 
-def rforest_vgg16_pca(X_train, y_train, X_test, input_shape=(256, 256, 3), n_components=500):
+    validacion(X_test, y_test_encoded, y_pred_rf)
+
+    return modelRF, scoresRF 
+
+def rforest_vgg16_pca(X_train, y_train_encoded, X_test, y_test_encoded, input_shape=(256, 256, 3), n_components=500):
     
     base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
     feature_extractor = Model(inputs=base_model.input, outputs=base_model.output)
@@ -54,10 +59,13 @@ def rforest_vgg16_pca(X_train, y_train, X_test, input_shape=(256, 256, 3), n_com
     )
 
     # Validación cruzada
-    scoresRF = cross_validate(modelRF, X_train_pca, y_train, cv=CV, scoring=scoring)
+    scoresRF = cross_validate(modelRF, X_train_pca, y_train_encoded, cv=CV, scoring=scoring)
 
-    modelRF.fit(X_train_pca, y_train)
-
+    modelRF.fit(X_train_pca, y_train_encoded)
+    y_pred_rf = modelRF.predict(X_test_pca)
+    
+    validacion(X_test, y_test_encoded, y_pred_rf)
+    
     # Precisión con VGG16 + PCA
     return modelRF, scoresRF
 
@@ -70,7 +78,7 @@ def extract_hog_features(images):
 
     return np.array(hog_features)
 
-def rforest_vgg16_pca_hog(X_train, y_train, X_test, X_train_gray, X_test_gray, input_shape=(256,256,3), n_components=500):
+def rforest_vgg16_pca_hog(X_train, y_train_encoded, X_test, y_test_encoded, X_train_gray, X_test_gray, input_shape=(64,64,3), n_components=500):
     base_model = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
     feature_extractor = Model(inputs=base_model.input, outputs=base_model.output)
 
@@ -101,11 +109,14 @@ def rforest_vgg16_pca_hog(X_train, y_train, X_test, X_train_gray, X_test_gray, i
     # Entrenar Random Forest con características combinadas
     model_rf_combined = RandomForestClassifier(n_estimators=200, max_depth=30, random_state=42)
     
-    cv_scores = cross_validate(model_rf_combined, X_train_combined, y_train, cv=CV, scoring=scoring)
-    print(f'Accuracy scores for each fold: {cv_scores}')
+    cv_scores = cross_validate(model_rf_combined, X_train_combined, y_train_encoded, cv=CV, scoring=scoring)
+    #print(f'Accuracy scores for each fold: {cv_scores}')
     #print(f'Mean cross-validation accuracy: {cv_scores.mean()}')
-    print(f'Mean cross-validation accuracy: {cv_scores["test_score"].mean()}')
+    #print(f'Mean cross-validation accuracy: {cv_scores["test_score"].mean()}')
 
-    model_rf_combined.fit(X_train_combined, y_train)
+    model_rf_combined.fit(X_train_combined, y_train_encoded)
+    y_pred_rf = model_rf_combined.predict(X_test_combined)
+
+    validacion(X_test, y_test_encoded, y_pred_rf)
 
     return model_rf_combined, cv_scores
